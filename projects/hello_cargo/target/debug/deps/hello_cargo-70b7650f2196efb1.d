50d69cb4fc354559bf4a160f7d55a28ed89c4fb7
Uku Reinkubjas <ukureinkubjas@gmail.com>
2024-09-24 18:28:29 +0300

    learning rust

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d
@@ -0,0 +1,5 @@
+/home/kooduser/github/Rust/projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1: src/main.rs
+
+/home/kooduser/github/Rust/projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-e8820e67b071f57e.d
@@ -0,0 +1,5 @@
+/home/kooduser/github/Rust/projects/hello_cargo/target/debug/deps/hello_cargo-e8820e67b071f57e: src/main.rs
+
+/home/kooduser/github/Rust/projects/hello_cargo/target/debug/deps/hello_cargo-e8820e67b071f57e.d: src/main.rs
+
+src/main.rs:
