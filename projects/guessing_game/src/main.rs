108eb9d9563b145448ad3763a2dd7c968fec9241
Uku Reinkubjas <ukureinkubjas@gmail.com>
2024-09-25 12:30:58 +0300

    first project

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,21 +7,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Secret number is {secret_number}");
-
-    println!("Enter your guess");
-
-    let mut guess = String::new();
-
-    io::stdin()
+    loop {
+        
+        println!("Enter your guess");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read the line");
 
-    println!("You guessed: {}", guess)
+        let guess: u32= match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>{
+             println!("You win!");
+             break;
+            }
+        }
     }
 }
+    
